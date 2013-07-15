0046a43fff2292272ea5fc03dd690ad36ab191d8
TB <t1@thomas-burg.de>
2013-07-16 02:48:42 +0300

    Created git repo Code has first changes by TB

---
+++ BasicDriveApp/RobotKitUI/ColorButtons.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Input;
+using Windows.UI;
+
+using BasicDriveApp;
+
+namespace BasicDriveApp
+{
+    class ColorButtons
+{
+
+        //! @brief	sphero to control
+        private RobotKit.Sphero m_sphero;
+
+        //! @brief  the last time a command was sent in milliseconds
+        private long m_lastCommandSentTimeMs;
+
+        /*!
+         * @brief	creates a joystick with the given @a puck element for a @a sphero
+         * @param	puck the puck to control with the joystick
+         * @param	sphero the sphero to control
+         */
+        public ColorButtons(RobotKit.Sphero sphero)
+        {
+            m_sphero = sphero;
+
+            m_lastCommandSentTimeMs = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+
+        public async void Anim1Clicked() {
+            // MainPage.SetColorAnimStopEnabled(true);
+            
+            // go through a few simple color changes
+            int red, green, blue;
+            for (int i = 0; i < 512; i++)
+            {
+                red = Math.Abs( (i * 1) % 512 - 256) ;
+                green = Math.Abs((i * 2) % 512 - 256);
+                blue = Math.Abs((i * 3) % 512 - 256);
+
+                m_sphero.SetRGBLED(red,green,blue);
+
+                // delay 10 ms
+                await Task.Delay(10);
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ BasicDriveApp/RobotKitUI/TB_Comments.cs
@@ -0,0 +1,16 @@
+﻿/*
+
+
+ * Change Log
+ * 
+ * Step 1 - Sun
+ * ============
+ * - changed title
+ * - changed size of color wheel from 512/512 to 256/256
+ * - added two buttons
+ * - made button "Anim1" work ;-)
+ * 
+ * 
+ * 
+
+*/
