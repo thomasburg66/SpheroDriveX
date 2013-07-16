7126d51e9ebddd14c72635ba8547c37592df937c
TB <t1@thomas-burg.de>
2013-07-16 03:11:58 +0300

    started work on SpheroSimul class, not working yet

---
+++ BasicDriveApp/RobotKitUI/SpheroSimul.cs
@@ -0,0 +1,24 @@
+﻿#region Assembly RobotKit.dll, v1.0.0.0
+// C:\Users\Thomas\Documents\Visual Studio 2013\Projects\spherotest1\BasicDriveApp\RobotKit.dll
+#endregion
+
+using RobotKit.Internal;
+using RobotKit;
+using System;
+using System.Diagnostics;
+
+namespace SpheroSimul1   
+{
+    public class SpheroSimul 
+    {
+
+        public SpheroSimul() {
+
+        }
+
+        private Sphero m_sphero;
+
+
+    }
+}
+
