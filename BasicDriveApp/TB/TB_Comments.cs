403cf29f1f62273e3a27fb3c7b4ccdbbb5ab23f1
thomasburg66 <t1@thomas-burg.de>
2013-07-18 18:09:27 +0300

    1.0.1.1: basic stuff works, further ideas see TB_Comments.cs

--- BasicDriveApp/TB/TB_Comments.cs
+++ BasicDriveApp/TB/TB_Comments.cs
@@ -1,5 +1,16 @@
 ﻿/*
 
+ * 
+ * Version displayed: see Package.appxmanifest, 
+ * 
+ * 
+ * 
+ *   <Identity Name="1c299fe1-c7cd-402c-8799-f93d6f534c55" Publisher="CN=WindowsSdk_2" Version="1.0.1.1" />
+  <Properties>
+    <DisplayName>SpheroDriveX</DisplayName>
+    <PublisherDisplayName>TB</PublisherDisplayName>
+    <Logo>Assets\StoreLogo.png</Logo>
+  </Properties>
 
  * todo/Change Log
  * 
@@ -9,7 +20,27 @@
  * Todo:
  * - intensity update broken
  * - calibration works but wheel is broken
+ * --> done :-)
+ * --> 1.0.1.1
  * 
+ * 1.0.1.1
+ * =======
+ * todo:
+ * - rearrange screen:
+ *   - save space with header (upper left: title, connection state, use simulator, ...)
+ *   - re-arrange lower part:
+ *     - color picker is nice
+ *     - decrease size of calibration wheel?
+ *     - increase size of steering wheel?
+ *     
+ * - add new controls:
+ *   - speed
+ *   
+ * - add new displays:
+ *   - simulator: 
+ *   - accelerator/gyrometer: display in graphics, not just numbers
+ *   
+ * bugs:
  * 
  * 
 
